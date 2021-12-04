90eff4468df3cf6b886d4fbe56947bd7daf7fcc7
huebye <thomashuebner94@gmx.de>
2021-12-04 13:33:33 +0100

    adjusted styling

--- src/components/about-view/about-view.jsx
+++ src/components/about-view/about-view.jsx
@@ -5,14 +5,14 @@ export class About extends React.Component {
 
   render () {
     return <div className='aboutSection'>
-     <p>The presented artworks are part of a private collection located in Berlin, Germany. <br />
-        In order to offer the beauty and excellence of the artworks and their creators to a wider audience the presentation of the collection takes place online. <br />
-
+     <p>The presented artworks are part of a private collection located in Berlin, Germany. <br /><br />
+        In order to offer the beauty and excellence of the artworks and their creators to a wider audience, the presentation of the collection takes place online. <br />
+      <br />
         We hope you can enjoy this idea and get to know artworks which would be hidden otherwise. <br />
 
-        In case of questions, please contact us: <br />
+        In case of questions, please contact us: 
 
-        anonymous@anonymouscollection.com <br />
+        anonymous@anonymouscollection.com <br /><br />
 
         Have a nice day and stay healthy!</p>
     </div>
